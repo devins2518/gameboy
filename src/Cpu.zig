78866ea9818f6b9fa0f8058d7fdb814c98f8d6c6
Devin Singh <drsingh2518@icloud.com>
2021-09-27 20:19:58 -0500

    fix defaults

--- src/Cpu.zig
+++ src/Cpu.zig
@@ -1,18 +1,13 @@
 const std = @import("std");
 const Self = @This();
 
-af: packed struct { a: u8, f: packed struct { _: u4, c: bool, h: bool, n: bool, z: bool } },
-bc: packed struct { b: u8, c: u8 },
-de: packed struct { d: u8, e: u8 },
-hl: packed struct { h: u8, l: u8 },
+af: packed struct { a: u8 = 0x00, f: packed struct { _: u4 = 0x0, c: u1 = 0, h: u1 = 0, n: u1 = 0, z: u1 = 0 } = .{} } = .{},
+bc: packed struct { b: u8 = 0x00, c: u8 = 0x00 } = .{},
+de: packed struct { d: u8 = 0x00, e: u8 = 0x00 } = .{},
+hl: packed struct { h: u8 = 0x00, l: u8 = 0x00 } = .{},
 
 fn init() Self {
-    return .{
-        .af = @bitCast(@TypeOf(.{}), 0u16),
-        .bc = @bitCast(@TypeOf(.{}), 0x0000),
-        .de = @bitCast(@TypeOf(.{}), 0x0000),
-        .hl = @bitCast(@TypeOf(.{}), 0x0000),
-    };
+    return .{};
 }
 
 test {
