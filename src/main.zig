471eff9720ecfb59f3d2b3b0d476785aa478fb9f
Devin Singh <drsingh2518@icloud.com>
2021-10-02 18:04:21 -0500

    fix path

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,9 @@ const Ppu = @import("Ppu.zig");
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 
 pub fn main() anyerror!void {
-    _ = getPath();
+    const path = getPath();
+    defer arena.allocator.free(path);
+    std.log.info("{s}", .{path});
 
     var bus = Bus.init(undefined);
     var ppu = Ppu.init();
@@ -28,12 +30,23 @@ pub fn main() anyerror!void {
 }
 
 fn getPath() []const u8 {
-    var args = std.process.args();
-    _ = args.skip();
-    // TODO: Is there a way to get the path of build.zig?
-    const path = args.next(arena.child_allocator) orelse "/home/devin/Repos/ziggyboy/roms/blargg/cpu_instrs/cpu_instrs.gb" catch unreachable;
+    const dirname = std.fs.path.dirname;
+    const join = std.fs.path.join;
+    const root = @src().file;
 
-    return path;
+    var args = std.process.args();
+
+    // Skip name
+    _ = args.skip();
+    const path = blk: {
+        if (args.next(arena.child_allocator)) |file| {
+            const path = file catch root;
+            break :blk path;
+        } else {
+            break :blk root;
+        }
+    };
+    return join(arena.child_allocator, &.{ dirname(dirname(path).?).?, "/roms/blargg/cpu_instrs/cpu_instrs.gb" }) catch root;
 }
 
 test {
