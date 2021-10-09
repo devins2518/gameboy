67baab244204d976bfb784b28bafda4d75d42510
Devin Singh <drsingh2518@icloud.com>
2021-10-08 23:00:23 -0500

    event loop

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const Cpu = @import("Cpu.zig");
 const Bus = @import("Bus.zig");
 const Ppu = @import("Ppu.zig");
 const Cartridge = @import("Cartridge.zig");
+const SDL = @import("sdl2");
 
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const blargg = getPath();
@@ -23,15 +24,54 @@ pub fn main() void {
     var cpu = Cpu.init(&bus, &ppu);
 
     var state = struct {
+        auto: bool = false,
         clocks: u64 = 0,
         bus: Bus,
         ppu: Ppu,
         cpu: Cpu,
+
+        fn step(self: *@This()) void {
+            nosuspend self.cpu.clock();
+            self.clocks += 1;
+        }
     }{ .bus = bus, .ppu = ppu, .cpu = cpu };
 
-    while (true) {
-        nosuspend cpu.clock();
-        state.clocks += 1;
+    if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO | SDL.SDL_INIT_EVENTS | SDL.SDL_INIT_AUDIO) < 0)
+        sdlPanic();
+    defer SDL.SDL_Quit();
+
+    var window = SDL.SDL_CreateWindow(
+        "ziggyboy",
+        SDL.SDL_WINDOWPOS_CENTERED,
+        SDL.SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL.SDL_WINDOW_SHOWN,
+    ) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyWindow(window);
+
+    var renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RENDERER_ACCELERATED) orelse sdlPanic();
+    defer _ = SDL.SDL_DestroyRenderer(renderer);
+
+    mainLoop: while (true) {
+        var ev: SDL.SDL_Event = undefined;
+        while (SDL.SDL_PollEvent(&ev) != 0) {
+            if ((ev.type == SDL.SDL_QUIT) or (ev.key.keysym.scancode == SDL.SDL_SCANCODE_ESCAPE))
+                break :mainLoop;
+            if (ev.key.keysym.scancode == SDL.SDL_SCANCODE_A)
+                state.auto = true;
+            if (ev.key.keysym.scancode == SDL.SDL_SCANCODE_G)
+                state.step();
+        }
+
+        _ = SDL.SDL_SetRenderDrawColor(renderer, 0xF7, 0xA4, 0x1D, 0xFF);
+        _ = SDL.SDL_RenderClear(renderer);
+
+        // if self.auto && frame_delta > clock_interval {
+        if (state.auto)
+            state.step();
+
+        SDL.SDL_RenderPresent(renderer);
     }
 
     arena.deinit();
@@ -44,6 +84,11 @@ pub fn getPath() []const u8 {
 
         return dn(dn(root).?).? ++ "/roms/blargg/cpu_instrs/cpu_instrs.gb";
     }
+}
+
+fn sdlPanic() noreturn {
+    const str = @as(?[*:0]const u8, SDL.SDL_GetError()) orelse "unknown error";
+    @panic(std.mem.sliceTo(str, 0));
 }
 
 test {
